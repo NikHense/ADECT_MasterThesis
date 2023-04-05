01066332c658f3cd4897b9d0dd37c0093602ad04
Niklas Henseling <niklas.henseling@outletcity.com>
2023-04-05 11:29:37 +0200

    Cleaning code acc. to Flake8

--- isolation_forest.py
+++ isolation_forest.py
@@ -1,16 +1,16 @@
 # %% Import libraries for isolation forest
-import os
+# import os
 import time
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
-from sklearn.inspection import DecisionBoundaryDisplay
+# from sklearn.inspection import DecisionBoundaryDisplay
 from numpy import bincount
 
 
 # %% Select columns for isolation forest
-total_payments_if = total_payments[['Payment_Number', 
+total_payments_if = total_payments[['Payment_Number',
                                     'Gen_Jnl_Line_Number',
                                     'Line_Number', 'ID_Vendor_Entry',
                                     'Object_Number', 'Vendor_Number',
@@ -40,31 +40,36 @@ for col in cat_cols:
     total_payments_if[col+'_encoded'] = total_payments_if[col].cat.codes
 
 
-# %% Create a binary encoding column for 'Review_Status' and 'Source_System' having 0 and 1
+# %% Create a binary encoding column
+# for 'Review_Status' and 'Source_System' having 0 and 1
 bin_cols = ['Review_Status', 'Source_System']
 for col in bin_cols:
     total_payments_if[col+'_encoded'] = total_payments_if[col].cat.codes
 
 # %% Convert 'Posting_Date' and 'Year-month' to integer
-total_payments_if['Posting_Date'] = total_payments_if['Posting_Date'].dt.strftime('%Y%m%d').astype(int)
-total_payments_if['Year-Month'] = total_payments_if['Year-Month'].dt.strftime('%Y%m').astype(int)
+total_payments_if['Posting_Date'] = total_payments_if[
+    'Posting_Date'].dt.strftime('%Y%m%d').astype(int)
+total_payments_if['Year-Month'] = total_payments_if['Year-Month'].dt.strftime(
+    '%Y%m').astype(int)
 
 
 # %% Select columns for isolation forest input
 # Select columns for isolation forest
 if_input = total_payments_if[['Payment_Number_encoded',
-                                    'Gen_Jnl_Line_Number',
-                                    'Line_Number', 'ID_Vendor_Entry',
-                                    'Object_Number', 'Vendor_Number',
-                                    'Country_Region_Code_encoded', 'Amount_Applied',
-                                    'Amount_Initial', 'Discount_Applied',
-                                    'Discount_Allowed', 'Discount_Rate',
-                                    'Discount_Possible', 'Payment_Method_Code_encoded',
-                                    'Customer_IBAN_encoded', 'Vendor_Bank_Origin_encoded',
-                                    'Vendor_IBAN_BIC_encoded', 'Posting_Date',
-                                    'Blocked_Vendor', 'Review_Status_encoded',
-                                    'Created_By_encoded', 'Source_System_encoded',
-                                    'Year-Month', 'Mandant_encoded']]
+                              'Gen_Jnl_Line_Number',
+                              'Line_Number', 'ID_Vendor_Entry',
+                              'Object_Number', 'Vendor_Number',
+                              'Country_Region_Code_encoded', 'Amount_Applied',
+                              'Amount_Initial', 'Discount_Applied',
+                              'Discount_Allowed', 'Discount_Rate',
+                              'Discount_Possible',
+                              'Payment_Method_Code_encoded',
+                              'Customer_IBAN_encoded',
+                              'Vendor_Bank_Origin_encoded',
+                              'Vendor_IBAN_BIC_encoded', 'Posting_Date',
+                              'Blocked_Vendor', 'Review_Status_encoded',
+                              'Created_By_encoded', 'Source_System_encoded',
+                              'Year-Month', 'Mandant_encoded']]
 
 if_input.info()
 # %% Create the inital isolation forest function
@@ -87,8 +92,8 @@ if_input.info()
 # print("Number of inliers: ", n_inliers)
 # %% Modify the isolation forest function
 starttime = time.time()
-isof = IsolationForest(n_estimators=1000, max_samples= 10000, contamination='auto', 
-                       random_state=42, verbose=0)
+isof = IsolationForest(n_estimators=1000, max_samples=10000,
+                       contamination='auto', random_state=42, verbose=0)
 
 isof.fit(if_input)
 
@@ -112,12 +117,15 @@ total_payments_if.insert(1, "anomaly1", anomaly1, True)
 
 # %% Get a subsample of the data frame with same distribution of anomalies
 # as the original data frame
-subsample = total_payments_if[total_payments_if['anomaly1'] == -1].sample(n=10, random_state=42)
-subsample = subsample.append(total_payments_if[total_payments_if['anomaly1'] == 1].sample(n=10, random_state=42))
+subsample = total_payments_if[total_payments_if['anomaly1']
+                              == -1].sample(n=10, random_state=42)
+subsample = subsample.append(total_payments_if[
+    total_payments_if['anomaly1'] == 1].sample(n=10, random_state=42))
 
-# get a subsample1 where all blocked vendor are inlcuded (blocked vendor is not 0)
-subsample1 = total_payments_if[total_payments_if['Blocked_Vendor'] != 0].sample(n=125, random_state=42)
+# get a subsample1 where all blocked vendor are inlcuded
+# (blocked vendor is not 0)
+subsample1 = total_payments_if[total_payments_if['Blocked_Vendor']
+                               != 0].sample(n=125, random_state=42)
 
 
 # %%
-
