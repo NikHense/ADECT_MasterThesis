8938b60e26bfd789182c8e17aa3e68082af74e5a
Niklas Henseling <niklas.henseling@outletcity.com>
2023-04-20 16:44:55 +0200

    update if with scaled data

--- Models/isolation_forest.py
+++ Models/isolation_forest.py
@@ -15,83 +15,17 @@ from statistics import mean
 # %% Start timer
 totaltime = time.time()
 
-# # %% Select columns for isolation forest
-# if_data = total_payments[['Payment_Number',
-#                                  'Gen_Jnl_Line_Number',
-#                                  'Line_Number', 'ID_Vendor_Entry',
-#                                  'Object_Number', 'Vendor_Number',
-#                                  'Country_Region_Code', 'Amount_Applied',
-#                                  'Amount_Initial', 'Discount_Applied',
-#                                  'Discount_Allowed', 'Discount_Rate',
-#                                  'Discount_Possible', 'Payment_Method_Code',
-#                                  'Customer_IBAN', 'Vendor_IBAN_BIC',
-#                                  'Vendor_Bank_Origin', 'Posting_Date',
-#                                  'Due_Date', 'Entry_Cancelled',
-#                                  'Blocked_Vendor', 'Review_Status',
-#                                  'Created_By', 'Source_System',
-#                                  'Year-Month', 'Mandant']]
-# # include index column in data frame for isolation forest
-# # if_data = if_data.reset_index()
-
-# # %% Print out the info of data frame
-# if_data.info()
-
-# # Show the decoded DataFrame
-# print(if_data)
-
-# # %% Convert the categories of to distinct integers using cat.codes
-# cat_cols = ['Payment_Number', 'Object_Number',
-#             'Country_Region_Code', 'Payment_Method_Code',
-#             'Customer_IBAN', 'Vendor_Bank_Origin', 'Vendor_IBAN_BIC',
-#             'Created_By',  'Mandant']
-# for col in cat_cols:
-#     if_data[col+'_encoded'] = if_data[col].cat.codes
-
-
-# # %% Create a binary encoding column
-# # for 'Review_Status' and 'Source_System' having 0 and 1
-# bin_cols = ['Review_Status', 'Source_System']
-# for col in bin_cols:
-#     if_data[col+'_encoded'] = if_data[col].cat.codes
-
-# # %% Convert 'Posting_Date' and 'Year-month' to integer
-# if_data['Posting_Date'] = if_data[
-#     'Posting_Date'].dt.strftime('%Y%m%d').astype(int)
-# if_data['Year-Month'] = if_data['Year-Month'].dt.strftime(
-#     '%Y%m').astype(int)
-
-
-# # %% Select columns for isolation forest input
-# # Select columns for isolation forest
-# if_data = if_data[['Payment_Number_encoded',
-#                            'Gen_Jnl_Line_Number',
-#                            'Line_Number', 'ID_Vendor_Entry',
-#                            'Object_Number_encoded', 'Vendor_Number',
-#                            'Country_Region_Code_encoded', 'Amount_Applied',
-#                            'Amount_Initial', 'Discount_Applied',
-#                            'Discount_Allowed', 'Discount_Rate',
-#                            'Discount_Possible',
-#                            'Payment_Method_Code_encoded',
-#                            'Customer_IBAN_encoded',
-#                            'Vendor_Bank_Origin_encoded',
-#                            'Vendor_IBAN_BIC_encoded', 'Posting_Date',
-#                            'Blocked_Vendor', 'Review_Status_encoded',
-#                            'Created_By_encoded', 'Source_System_encoded',
-#                            'Year-Month', 'Mandant_encoded']]
-
-# if_data.info()
-
 # # %% Create the inital isolation forest function
 # starttime = time.time()
 # isof = IsolationForest(n_estimators=1000, max_samples=10000,
 #                        contamination='auto', random_state=42,
 #                        verbose=0, n_jobs=-1)
 
-# isof.fit(if_data)
+# isof.fit(input)
 
-# scores1 = isof.decision_function(if_data)
+# scores1 = isof.decision_function(input)
 
-# anomaly1 = isof.predict(if_data)
+# anomaly1 = isof.predict(input)
 
 # # print(y_pred)
 # #  Results of the isolation forest
@@ -103,15 +37,15 @@ totaltime = time.time()
 # print(f'Database read process took {time.time() - starttime} seconds')
 # # %% add the scores1 and anomaly1 values to the respective rows
 # # in the data frame to position 0
-# if_data.insert(0, "scores1", scores1, True)
-# if_data.insert(1, "anomaly1", anomaly1, True)
+# input.insert(0, "scores1", scores1, True)
+# input.insert(1, "anomaly1", anomaly1, True)
 
 # %% Execute grid search for isolation forest (!!! 8 min running time !!!)
 starttime = time.time()
 
 # Define list of parameter values to test
-n_estimators_list = list(range(100, 2002, 100))
-max_samples_list = list(range(500, len(if_data), 500))
+n_estimators_list = list(range(100, 1002, 100))
+max_samples_list = list(range(500, len(input_scaled), 500))
 
 # Initialize empty lists to store results
 results = []
@@ -125,9 +59,9 @@ def fit_isof(params):
     isof = IsolationForest(n_estimators=n_estimators, max_samples=max_samples,
                            contamination='auto', random_state=42,
                            verbose=0, n_jobs=1)
-    isof.fit(if_data)
-    # scores = isof.decision_function(if_data)
-    anomaly = isof.predict(if_data)
+    isof.fit(input_scaled)
+    # scores = isof.decision_function(input_scaled)
+    anomaly = isof.predict(input_scaled)
     n_outliers = bincount((anomaly == -1).astype(int))[1]
     n_inliers = bincount((anomaly == 1).astype(int))[1]
     # elapsed_time1 = time.time() - starttime1
@@ -208,11 +142,17 @@ avg_outlier = np.array(avg_outlier)
 x = max_samples_list
 
 # %%
-kneedle = KneeLocator(x, avg_outlier,
+kneedle = KneeLocator(x, avg_outlier, interp_method='polynomial',
                       curve='convex', direction='decreasing')
 
 print(round(kneedle.elbow, 0))
 print(round(kneedle.elbow_y, 3))
+
+plt.style.use('ggplot')
+kneedle.plot_knee_normalized()
+# plt.xlim(0, 0.05)
+# plt.ylim(0.95, 1)
+plt.show()
 
 # Plot the k-distance graph with the knee point (zoomed in)
 plt.figure(figsize=(10, 10))
@@ -230,26 +170,25 @@ plt.ylim(kneedle.elbow_y - 500, kneedle.elbow_y + 1000)
 plt.show()
 
 # %% Create the isolation forest with tuned parameter
-
 starttime = time.time()
 
 avg_n_estimator = mean(n_estimators_list)
-max_samples = int(kneedle.elbow)
 
-isof2 = IsolationForest(n_estimators=avg_n_estimator, max_samples=max_samples,
-                       contamination='auto', random_state=42,
-                       verbose=0, n_jobs=-1)
+isof = IsolationForest(n_estimators=avg_n_estimator,
+                        max_samples=int(kneedle.elbow),
+                        contamination='auto', random_state=42,
+                        verbose=0, n_jobs=-1)
 
-isof2.fit(if_data)
+isof.fit(input_scaled)
 
-scores = isof2.decision_function(if_data)
+if_scores = isof.decision_function(input_scaled)
 
-anomaly = isof2.predict(if_data)
+if_anomaly = isof.predict(input_scaled)
 
 
 #  Results of the isolation forest
-n_outliers = bincount((anomaly == -1).astype(int))[1]
-n_inliers = bincount((anomaly == 1).astype(int))[1]
+n_outliers = bincount((if_anomaly == -1).astype(int))[1]
+n_inliers = bincount((if_anomaly == 1).astype(int))[1]
 
 print("Number of outliers: ", n_outliers)
 print("Number of inliers: ", n_inliers)
@@ -257,12 +196,12 @@ print(f'Database read process took {time.time() - starttime} seconds')
 
 # %%
 # Convert the dbscan_output array to a pandas DataFrame
-if_output = pd.DataFrame(if_data, columns=if_data.columns)
+if_output = pd.DataFrame(input_scaled, columns=input.columns)
 
 # Add the labels column to the dbscan_output at position 0
-if_output.insert(0, 'INDEX', if_data.index)
-if_output.insert(1, 'labels', anomaly, True)
-if_output.insert(2, "scores", scores, True)
+if_output.insert(0, 'INDEX', input.index)
+if_output.insert(1, 'labels', if_anomaly, True)
+if_output.insert(2, "scores", if_scores, True)
 
 # Filter out the a data frame with only noise points & clean
 if_noise = if_output[if_output['labels'] == -1]
@@ -272,8 +211,5 @@ minutes = int((time.time() - totaltime) / 60)
 seconds = int((time.time() - totaltime) % 60)
 print(f'Isolation Forest process took {minutes} minutes and '
       f'{seconds} seconds')
-
-
-
 
 # %%
